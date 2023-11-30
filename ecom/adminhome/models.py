ce99c917cae65f511cd77096fb191251eb3f63a3
Mohammed Shan <shanmohamme.123@gmail.com>
2023-11-30 13:00:07 +0530

    commit in master befor deleting old template and add new

--- ecom/adminhome/models.py
+++ ecom/adminhome/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    brand_image = models.ImageField(upload_to='Brand/')
+    brand_name = models.CharField(max_length=40)
+    def __str__(self):
+        return self.brand_name
+    
+class Category(models.Model):
+    Category_image = models.ImageField(upload_to='Category/')
+    Category_name = models.CharField(max_length=40)    
+    def __str__(self):
+        return self.Category_name
+    
+class Products(models.Model):
+    product_name = models.CharField(max_length=50)
+    product_image = models.ImageField(upload_to='products/')
+    product_des = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    user_gender  =  models.CharField(max_length=10)
+    product_price = models.BigIntegerField()
+    quantity = models.IntegerField()
+    status = models.BooleanField()
+    tag = models.CharField(max_length=20)
+    def __str__(self):
+        return self.product_name
